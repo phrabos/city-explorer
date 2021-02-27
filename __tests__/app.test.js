f2c490b5319858edbb7921dea468d7422560d39c
patrick hrabos <phrabos@gmail.com>
2021-02-26 21:40:10 -0500

    pass test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,46 +1,49 @@
 require('dotenv').config();
 
-const { execSync } = require('child_process');
+const { mungeReviews } = require('../lib/munge-utils');
 
-const fakeRequest = require('supertest');
-const app = require('../lib/app');
-const client = require('../lib/client');
+test('returns a review from Yelp API', async() => {
 
-describe('app routes', () => {
-  describe('routes', () => {
-    let token;
+  const expectation = [
+    { 
+      'name': 'Owens Fish Camp', 
+      'image_url': 'https://s3-media3.fl.yelpcdn.com/bphoto/cIEYVhc7dMB-vIh_Tvv9gQ/o.jpg',
+      'price': '$$',
+      'rating': 4.5,
+      'url': 'https://www.yelp.com/biz/owens-fish-camp-sarasota?adjust_creative=iB-g85KitIPLvFXHo4hWFg&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=iB-g85KitIPLvFXHo4hWFg'
+    }
+  ]; 
   
-    beforeAll(async done => {
-      execSync('npm run setup-db');
-  
-      client.connect();
-  
-      const signInData = await fakeRequest(app)
-        .post('/auth/signup')
-        .send({
-          email: 'jon@user.com',
-          password: '1234'
-        });
-      
-      token = signInData.body.token; // eslint-disable-line
-  
-      return done();
-    });
-  
-    afterAll(done => {
-      return client.end(done);
-    });
-
-    test('returns animals', async() => {
-
-      const expectation = { 'formatted_query': 'Sibay, городской округ Сибай, Bashkortostan, Volga Federal District, UNDEFINED, Russia', 'latitude': '52.7206093188075', 'longitude': '58.6657536770277' };
-
-      const data = await fakeRequest(app)
-        .get('/location')
-        .expect('Content-Type', /json/)
-        .expect(200);
-
-      expect(data.body).toEqual(expectation);
-    });
-  });
-});
+  const mungie = { 
+    'businesses': 
+    [
+      { 
+        'id': 'USq57A18KfPeheWDfFzVkA',
+        'alias': 'owens-fish-camp-sarasota',
+        'name': 'Owens Fish Camp',
+        'image_url': 'https://s3-media3.fl.yelpcdn.com/bphoto/cIEYVhc7dMB-vIh_Tvv9gQ/o.jpg',
+        'is_closed': false,
+        'url': 'https://www.yelp.com/biz/owens-fish-camp-sarasota?adjust_creative=iB-g85KitIPLvFXHo4hWFg&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=iB-g85KitIPLvFXHo4hWFg',
+        'review_count': 1656,
+        'categories': [{ 'alias': 'seafood', 'title': 'Seafood' }],
+        'rating': 4.5,
+        'coordinates': { 'latitude': 27.3319,
+          'longitude': -82.53974 },
+        'transactions': ['delivery'],
+        'price': '$$',
+        'location': { 'address1': '516 Burns Ln',
+          'address2': '',
+          'address3': '',
+          'city': 'Sarasota',
+          'zip_code': '34236',
+          'country': 'US',
+          'state': 'FL',
+          'display_address': ['516 Burns Ln', 
+            'Sarasota, FL 34236'] },
+        'phone': '+19419516936',
+        'display_phone': '(941) 951-6936',
+        'distance': 1029.578186539982 
+      }
+    ] 
+  }; 
+  const actual = mungeReviews(mungie); expect(actual).toEqual(expectation); });
